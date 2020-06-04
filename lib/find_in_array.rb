5fbbc136918eed1177b46d5b45d4d8557525a1a5
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-04 15:24:50 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
 
     for (i = 0; i < array.length; i++)
       if value_to_find[i] == value_to_find[i]
-      valut_to_find
+      value_to_find
     else
       value_to_find[i] == nil
       nil
